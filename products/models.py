8ea2a0f052d7d784d8cf3ce6e0349abcca1f27ee
TL <thuycis@gmail.com>
2024-06-27 14:00:12 -0700

    feat(newproject): GET product list

---
+++ products/models.py
@@ -0,0 +1,11 @@
+# products/models.py
+
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    sku = models.CharField(max_length=100, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
---
+++ products/urls.py
@@ -0,0 +1,8 @@
+# products/urls.py
+
+from django.urls import path
+from .views import ProductListView
+
+urlpatterns = [
+    path('products/', ProductListView.as_view(), name='product-list'),
+]
